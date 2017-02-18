6a5d5c7a824f1bf8ddbfe90333c3c4c1ece72abe
Huanhuan Dong <donghuanhuan1988@hotmail.com>
2017-02-18 19:17:27 +0800

    Bobby Q1

---
+++ Bobby/Q1/js/site.js
@@ -0,0 +1,47 @@
+
+function onPageLoad() {
+    gXmlhttpRequest = new XMLHttpRequest();
+    gXmlhttpRequest.onreadystatechange = function () {
+        if (null == gXmlhttpRequest || 4 != gXmlhttpRequest.readyState || 200 != gXmlhttpRequest.status) {
+            return;
+        }
+        var pageStatus = document.getElementById('status');
+        pageStatus.parentNode.removeChild(pageStatus);
+        var json = JSON.parse(gXmlhttpRequest.responseText)
+        displayData(json);
+    }
+
+    gXmlhttpRequest.open('GET', `https://api.github.com/search/repositories?q=javascript`);
+    gXmlhttpRequest.send();
+}
+
+function displayData(data) {
+    if (null == data) {
+        return;
+    }
+
+    var table = document.getElementById('contentTable');
+    if (null == table) {
+        return;
+    }
+
+    table.setAttribute('border', '2');
+
+    var maxCount = 10;
+    var count = 0;
+    var reposInOneRow = 2;
+    var newRow = null;
+    for (var index in data.items) {
+        if (count % reposInOneRow == 0) {
+            newRow = table.insertRow();
+        }
+
+        var newCell = newRow.insertCell();
+        newCell.innerHTML = data.items[index]['full_name'] + '<br />'+ data.items[index]['description'];
+
+        ++count;
+        if (count >= maxCount) {
+            return;
+        }
+    }
+}
