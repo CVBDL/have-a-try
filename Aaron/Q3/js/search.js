321502a04ae8007e5838f439c7b7346a68d8a778
Awang14 <305368985@qq.com>
2017-02-27 14:27:26 +0800

    Q3 update

--- Aaron/Q3/js/search.js
+++ Aaron/Q3/js/search.js
@@ -1,14 +1,14 @@
 'use strict'
 
 $(document).ready(function(){
-  	$("form").submit(function(e){
-	  	var keyword = document.getElementById("searchbox").value;
-	  	var newurl = "https://github.com/search?utf8=%E2%9C%93&q=" + keyword;
-		$.ajax({
-			url: newurl,
-			//data:{Full:"fu"},
-			type: "GET",
-			dataType:'json',
-			});
-  	});
+	$("#myForm").on("submit",function(ev){
+		ev.preventDefault();
+	});
 });
+function clicksearch(){
+	$.ajax({
+		type: "GET",
+        url: "https://api.github.com/search/repositories?q="+$("#searchbox").val(),
+		dataType:'json'
+		});
+}
