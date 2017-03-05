e68a0306518ca32b701b739a485c87eba97b2b7e
Aaron <305368985@qq.com>
2017-03-05 22:32:51 +0800

    update Q4

--- Aaron/Q3/Solution2/js/search.js
+++ Aaron/Q3/Solution2/js/search.js
@@ -1,38 +1,39 @@
 'use strict'
 
-$(document).ready(function(){
-	$("#myForm").on("submit",function(ev){
+$(document).ready(function() {
+	$("#myForm").on("submit", function(ev) {
 		ev.preventDefault();
 	});
 });
-function clicksearch(){
+
+function clicksearch() {
 	$("#myDiv").empty();
 	$("#repo_list").empty();
 	$.ajax({
 		type: "GET",
-        url: "https://api.github.com/search/repositories?q="+$("#searchbox").val(),
-		dataType:'json',
-		beforeSend:function(XMLHttpRequest){
-         	$("#loading").html("<img src='img/loading.gif' />"); 
-         }, 
-		success: function(msg){
-	   	$("#myDiv").html("<h2>Top 10 repositories!</h2>");
-	   	var htmlstr=""
-		for(var i=0;i<10;i++){
-		htmlstr+='<div class="row"><div class="col s12 m7"><div class="small card"><div class="card-image"><img src="./img/'+i+'.jpg"><span class="card-title" id="title'+i+'"></span></div><div class="card-content" id="content'+i+'"><p></p></div><div class="card-action"><a id="link'+i+'" href="#">Link To</a></div></div></div></div>'
-		}
-		$("#repo_list").html(htmlstr);
- 		$(".row").each(function(i){
-		$("#title"+i).text(msg.items[i].full_name);
-		$("#content"+i).text(msg.items[i].description);
-		$("#link"+i).attr("href",msg.items[i].html_url); 
-		});
+		url: "https://api.github.com/search/repositories?q=" + $("#searchbox").val(),
+		dataType: 'json',
+		beforeSend: function(XMLHttpRequest) {
+			$("#loading").html("<img src='img/loading.gif' />");
 		},
-		complete:function(XMLHttpRequest,textStatus){ 
-             $("#loading").empty(); 
-        }, 
-		error: function(data, status, e){ 
-     	alert("error"); 
-   		},
+		complete: function(XMLHttpRequest, textStatus) {
+			$("#loading").empty();
+		},
+		success: function(msg) {
+			$("#myDiv").html("<h2>Top 10 repositories!</h2>");
+			var htmlstr = ""
+			for(var i = 0; i < 10; i++) {
+				htmlstr += '<div class="row"><div class="col s12 m7"><div class="small card"><div class="card-image"><img src="./img/' + i + '.jpg"><span class="card-title" id="title' + i + '"></span></div><div class="card-content" id="content' + i + '"><p></p></div><div class="card-action"><a id="link' + i + '" href="#">Link To</a></div></div></div></div>'
+			}
+			$("#repo_list").html(htmlstr);
+			$(".row").each(function(i) {
+				$("#title" + i).text(msg.items[i].full_name);
+				$("#content" + i).text(msg.items[i].description);
+				$("#link" + i).attr("href", msg.items[i].html_url);
+			});
+		},
+		error: function(data, status, e) {
+			alert("error");
+		},
 	});
 }
