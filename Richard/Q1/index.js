c1d6e4020a8a273c1b9d9a4b8fa77f4464f47471
Peisong <PLi5@rockwellautomation.com>
2017-03-08 16:37:45 +0800

    test

--- Richard/Q1/index.js
+++ Richard/Q1/index.js
@@ -2,25 +2,37 @@
  * @Author: Peisong
  * @Date:   2017-02-23 16:48:35
  * @Last Modified by:   Peisong
- * @Last Modified time: 2017-03-02 16:56:52
+ * @Last Modified time: 2017-03-08 16:34:24
  */
 
 'use strict';
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
+    $scope.searchContent = "";
+    $scope.isWarningShow = false;
     $scope.infos = [];
     $scope.num = 10;
     $scope.isLoadingShow = false;
+
     $scope.GetRepo = () => {
-        $http.get("https://api.github.com/search/repositories?q=javascript").success((data) => {
+        if ($scope.searchContent != "") {
+            $scope.isWarningShow = false;
+
             $scope.isLoadingShow = true;
-            $scope.infos = data.items;
-        });
+            var myUrl = "https://api.github.com/search/repositories?q=" + $scope.searchContent;
+            $http.get(myUrl)
+                .success((data) => {
+                    $scope.isLoadingShow = false;
+                    $scope.infos = data.items;
+                })
+                .error(() => {
+                    $scope.isLoadingShow = false;
+                });
+        } else {
+            $scope.isWarningShow = true;
+            $scope.isLoadingShow = true;
+        }
     }
-
-
-
-    $scope.GetRepo();
 });
 
 function resize() {
