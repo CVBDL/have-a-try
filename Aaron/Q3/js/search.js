50f9f2f545b9fd3fbfe100a3bc0c73429c04df72
Awang14 <305368985@qq.com>
2017-03-10 16:54:02 +0800

    update

--- Aaron/Q3/js/search.js
+++ Aaron/Q3/js/search.js
@@ -1,28 +1,29 @@
 'use strict'
 
-$(document).ready(function(){
-	$("#myForm").on("submit",function(ev){
-		ev.preventDefault();
-	});
+$(document).ready(function() {
+  $("#myForm").on("submit", function(ev) {
+    ev.preventDefault();
+  });
 });
-function clicksearch(){
-	$.ajax({
-		type: "GET",
-        url: "https://api.github.com/search/repositories?q="+$("#searchbox").val(),
-		dataType:'json',
-		success: function(msg){
-	   	$("#myDiv").html("<h2>Top 10 repositories!</h2>");
-	   	var htmlstr=""
-		for(var i=0;i<10;i++){
-		htmlstr+='<div class = "row" id="'+i+'"></div>'
-		}
-		$("#repo_list").html(htmlstr);
-   		$(".row").each(function(i){
-		$(this).html(msg.items[i].full_name + "<br>" + msg.items[i].description);
-		});
-		},
-		error: function(data, status, e){ 
-     	alert("error"); 
-     	}
-	});
+
+function clicksearch() {
+  $.ajax({
+    type: "GET",
+    url: "https://api.github.com/search/repositories?q=" + $("#searchbox").val(),
+    dataType: 'json',
+    success: function(msg) {
+      $("#myDiv").html("<h2>Top 10 repositories!</h2>");
+      var htmlstr = ""
+      for(var i = 0; i < 10; i++) {
+        htmlstr += '<div class = "row" id="' + i + '"></div>'
+      }
+      $("#repo_list").html(htmlstr);
+      $(".row").each(function(i) {
+        $(this).html(msg.items[i].full_name + "<br>" + msg.items[i].description);
+      });
+    },
+    error: function(data, status, e) {
+      alert("error");
+    }
+  });
 }
