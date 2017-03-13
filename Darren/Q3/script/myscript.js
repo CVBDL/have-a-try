7f9c3baf6364920c82f5516c7be5b526564b66f3
DarrenTeng <tengyingzheng@sina.com>
2017-03-13 18:24:49 +0800

    update Q3 script

--- Darren/Q3/script/myscript.js
+++ Darren/Q3/script/myscript.js
@@ -1,15 +1,4 @@
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-	var searchtext = document.getElementById("search_text");
-	
-  var search = "https://api.github.com/search/repositories?q=" + searchtext.value;
-  xhttp.open("GET", search, true);
-  xhttp.send();
   
   var argKeyword = $("#search_text").val();
     if(null == argKeyword){
