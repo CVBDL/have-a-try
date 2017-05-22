901e3a7b5e09f5e70509e79c0591e273f0beb7a2
Aaron <305368985@qq.com>
2017-05-22 23:14:05 +0800

    update

--- Aaron/btts_26_iio/assets/js/custom.js
+++ Aaron/btts_26_iio/assets/js/custom.js
@@ -35,7 +35,29 @@
         mainApp.slide_fun();
         mainApp.dataTable_fun();
         mainApp.custom_fun();
-    });
+
+        var myApp = angular.module("myApp", [&apos;ui.router&apos;]);
+        myApp.config(function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.when("", "/index");
+            $stateProvider
+        .state("index", {
+            url: "/index",
+            templateUrl: "index.html"
+        })
+        .state("index.pc", {
+            url:"/pc",
+            templateUrl: "pc.html"
+        })
+        .state("index.mobile", {
+            url:"/mobile",
+            templateUrl: "mobile.html"
+        })
+        .state("index.vm", {
+            url:"/vm",
+            templateUrl: "vm.html"
+        });
+        });
+});
 }(jQuery));
 
 
