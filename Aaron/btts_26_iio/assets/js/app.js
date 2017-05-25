dd4569e3fdbdee1c69973d214320088bbf79cb0f
Awang14 <305368985@qq.com>
2017-05-25 18:09:39 +0800

    update

--- Aaron/btts_26_iio/assets/js/app.js
+++ Aaron/btts_26_iio/assets/js/app.js
@@ -1,21 +1,61 @@
-var myApp = angular.module("myApp", ['ui.router']);
+var myApp = angular.module("myApp", ['ui.router', 'ui.bootstrap']);
         myApp.config(function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.when("", "/pa");
+            $urlRouterProvider.when("", "/dash");
             $stateProvider
-        .state("pa", {
-            url: "/pa",
-            templateUrl: "pa.html"
+        .state("dash", {
+            url: "/dash",
+            templateUrl: "dash.html"
         })
-        .state("pa.p1", {
-            url:"/p1",
-            templateUrl: "p1.html"
+        .state("dash.pc", {
+            url:"/pc",
+            templateUrl: "pc.html"
         })
-        .state("pa.p2", {
-            url:"/p2",
-            templateUrl: "p2.html"
+        .state("dash.mobile", {
+            url:"/mobile",
+            templateUrl: "mobile.html"
         })
-        .state("pa.p3", {
-            url:"/p3",
-            templateUrl: "p3.html"
+        .state("dash.vm", {
+            url:"/vm",
+            templateUrl: "vm.html"
+        })
+        .state("dash.mac", {
+            url:"/mac",
+            templateUrl: "mac.html"
         });
+        
+        // angular.module('app', ['ui.bootstrap']);
+        // function CarouselDemoCtrl($scope){
+        //   $scope.myInterval = 3000;
+        //   $scope.slides = [
+        //     {
+        //       image: './assets/img/1.jpg'
+        //     },
+        //     {
+        //       image: 'assets/img/1.jpg'
+        //     },
+        //     {
+        //       image: 'assets/img/1.jpg'
+        //     },
+        //     {
+        //       image: 'assets/img/1.jpg'
+        //     }
+        //   ];
+        // }
+
+        myApp.controller('CarouselDemoCtrl', function($scope){
+            $scope.myInterval = 3000;
+            var slides = $scope.slides = [];
+            slides.push({image:'./assets/img/1.jpg'});
+            slides.push({image:'./assets/img/2.jpg'});
+        });
+
+      //   myApp.controller('CarouselDemoCtrl', function ($scope) {
+      //     $scope.myInterval = 5000;
+      //     $scope.noWrapSlides = false;
+      //     $scope.active = 0;
+      //     var slides = $scope.slides = [];
+      //     slides.push({image:'./assets/img/1.jpg'});
+      //     slides.push({image:'./assets/img/2.jpg'});
+      //     var currIndex = 0;
+      // });
 });
