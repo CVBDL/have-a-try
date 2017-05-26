ced34fcff6a5cff142e95a70b6cb34b05cc0b31f
Awang14 <305368985@qq.com>
2017-05-26 17:09:05 +0800

    update

--- Aaron/btts_26_iio/assets/js/app.js
+++ Aaron/btts_26_iio/assets/js/app.js
@@ -1,27 +1,28 @@
 var myApp = angular.module("myApp", ['ui.router', 'ui.bootstrap']);
         myApp.config(function ($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.when("", "/dash");
+            
             $stateProvider
-        .state("dash", {
-            url: "/dash",
-            templateUrl: "dash.html"
-        })
-        .state("dash.pc", {
-            url:"/pc",
-            templateUrl: "pc.html"
-        })
-        .state("dash.mobile", {
-            url:"/mobile",
-            templateUrl: "mobile.html"
-        })
-        .state("dash.vm", {
-            url:"/vm",
-            templateUrl: "vm.html"
-        })
-        .state("dash.mac", {
-            url:"/mac",
-            templateUrl: "mac.html"
-        });
+		        .state("dash", {
+		            url: "/dash",
+		            templateUrl: "dash.html"
+		        })
+		        .state("dash.pc", {
+		            url:"/pc",
+		            templateUrl: "pc.html"
+		        })
+		        .state("dash.mobile", {
+		            url:"/mobile",
+		            templateUrl: "mobile.html"
+		        })
+		        .state("dash.vm", {
+		            url:"/vm",
+		            templateUrl: "vm.html"
+		        })
+		        .state("dash.mac", {
+		            url:"/mac",
+		            templateUrl: "mac.html"
+		        });
         
         // angular.module('app', ['ui.bootstrap']);
         // function CarouselDemoCtrl($scope){
@@ -42,11 +43,11 @@ var myApp = angular.module("myApp", ['ui.router', 'ui.bootstrap']);
         //   ];
         // }
 
-        // myApp.controller('CarouselDemoCtrl', function($scope){
-        //     $scope.myInterval = 3000;
-        //     var slides = $scope.slides = [];
-        //     slides.push({image:'./assets/img/1.jpg'});
-        //     slides.push({image:'./assets/img/2.jpg'});
-        // });
+           myApp.controller('CarouselDemoCtrl', function($scope){
+               $scope.myInterval = 3000;
+               var slides = $scope.slides = [];
+               slides.push({image:'./assets/img/1.jpg'});
+               slides.push({image:'./assets/img/2.jpg'});
+           });
 
 });
