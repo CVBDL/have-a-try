db288ba4a28cb2f3b15b6b8cde50c0e0c222f00c
Awang14 <305368985@qq.com>
2017-06-01 15:23:56 +0800

    update

--- Aaron/btts_26_iio/assets/js/app.js
+++ Aaron/btts_26_iio/assets/js/app.js
@@ -1,83 +1,86 @@
 var myApp = angular.module("myApp", ['ui.router', 'ui.bootstrap', 'smart-table']);
 // UI-Router
 myApp.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.when("", "/dash");
+    $urlRouterProvider.when("", "/dash");
 
-  $stateProvider
-    .state("dash", {
-      url: "/dash",
-      templateUrl: "dash.html"
-    })
-    .state("dash.pc", {
-      url: "/pc",
-      templateUrl: "pc.html"
-    })
-    .state("dash.mobile", {
-      url: "/mobile",
-      templateUrl: "mobile.html"
-    })
-    .state("dash.vm", {
-      url: "/vm",
-      templateUrl: "vm.html"
-    })
-    .state("dash.mac", {
-      url: "/mac",
-      templateUrl: "mac.html"
+    $stateProvider.state("dash", {
+        url: "/dash",
+        templateUrl: "dash.html"
+    }).state("dash.pc", {
+        url: "/pc",
+        templateUrl: "pc.html"
+    }).state("dash.mobile", {
+        url: "/mobile",
+        templateUrl: "mobile.html"
+    }).state("dash.vm", {
+        url: "/vm",
+        templateUrl: "vm.html"
+    }).state("dash.mac", {
+        url: "/mac",
+        templateUrl: "mac.html"
     });
 });
 // Slide Show
 myApp.controller('CarouselDemoCtrl', function($scope) {
-  $scope.myInterval = 5000;
-  var slides = $scope.slides = [];
-  slides.push({ image: 'assets/img/1.jpg' }, { image: 'assets/img/2.jpg' }, { image: 'assets/img/3.jpg' });
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    slides.push({
+        image: 'assets/img/1.jpg'
+    }, {
+        image: 'assets/img/2.jpg'
+    }, {
+        image: 'assets/img/3.jpg'
+    });
 });
 
-myApp.controller('basicsCtrl', function($scope,$http) {
-  // function basicsCtrl($scope,$http) {
-    var url="assets/data/vm.json";
-    $http.get(url).success( function(response) {
+myApp.controller('basicsCtrl', function($scope, $http) {
+    var url = "assets/data/vm.json";
+    // Remove Row
+    $scope.removeRow = function removeRow(row) {
+        var index = $scope.rowCollection.indexOf(row);
+        if (index !== -1) {
+            $scope.rowCollection.splice(index, 1);
+        }
+    };
+
+    $scope.itemsByPage = 5;
+
+    $http.get(url).success(function(response) {
+        $scope.row123 = response;
         $scope.rowCollection = response;
-        console.log($scope.rowCollection.length); 
+        $scope.abc = url;
+        console.log(11, $scope.rowCollection);
+
+        // Add Owner List
+        var owner_list = [];
+        for (var i = 0; i < $scope.rowCollection.length; i++) {
+            owner_list[i] = $scope.rowCollection[i].owner;
+        }
+
+        function unique(arr) {
+            var result = []
+              , hash = {};
+            for (var i = 0, elem; (elem = arr[i]) != null; i++) {
+                if (!hash[elem]) {
+                    result.push(elem);
+                    hash[elem] = true;
+                }
+            }
+            return result;
+        }
+        var arr = unique(owner_list);
+        $scope.activities = arr;
     });
 
-  // Add Owner List
-  // var owner_list = [];
-  // for(var i = 0; i < $scope.rowCollection.length; i++) {
-  //   owner_list[i] = $scope.rowCollection[i].owner;
-  // }
-
-  // function unique(arr) {
-  //   var result = [],
-  //     hash = {};
-  //   for(var i = 0, elem;
-  //     (elem = arr[i]) != null; i++) {
-  //     if(!hash[elem]) {
-  //       result.push(elem);
-  //       hash[elem] = true;
-  //     }
-  //   }
-  //   return result;
-  // }
-  // var arr = unique(owner_list);
-  // $scope.activities = arr;
-
-  // Remove Row
-  $scope.removeRow = function removeRow(row) {
-    var index = $scope.rowCollection.indexOf(row);
-    if(index !== -1) {
-      $scope.rowCollection.splice(index, 1);
-    }
-  };
-  $scope.itemsByPage = 5;
 });
 
 myApp.directive('stRatio', function() {
-  return {
-    link: function(scope, element, attr) {
-      var ratio = +(attr.stRatio);
+    return {
+        link: function(scope, element, attr) {
+            var ratio = +(attr.stRatio);
 
-      element.css('width', ratio + '%');
+            element.css('width', ratio + '%');
 
-    }
-  };
+        }
+    };
 });
--- Aaron/btts_26_iio/assets/js/test.js
+++ Aaron/btts_26_iio/assets/js/test.js
@@ -1,29 +1,88 @@
-app.controller('paginationCtrl', ['$scope', function (scope) {
-    var
-        nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'],
-        familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'];
+var myApp = angular.module("myApp", ['ui.router', 'ui.bootstrap', 'smart-table']);
+// UI-Router
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/dash");
 
-    function createRandomItem() {
-        var
-            firstName = nameList[Math.floor(Math.random() * 4)],
-            lastName = familyName[Math.floor(Math.random() * 4)],
-            age = Math.floor(Math.random() * 100),
-            email = firstName + lastName + '@whatever.com',
-            balance = Math.random() * 3000;
+    $stateProvider.state("dash", {
+        url: "/dash",
+        templateUrl: "dash.html"
+    }).state("dash.pc", {
+        url: "/pc",
+        templateUrl: "pc.html"
+    }).state("dash.mobile", {
+        url: "/mobile",
+        templateUrl: "mobile.html"
+    }).state("dash.vm", {
+        url: "/vm",
+        templateUrl: "vm.html"
+    }).state("dash.mac", {
+        url: "/mac",
+        templateUrl: "mac.html"
+    });
+});
+// Slide Show
+myApp.controller('CarouselDemoCtrl', function($scope) {
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    slides.push({
+        image: 'assets/img/1.jpg'
+    }, {
+        image: 'assets/img/2.jpg'
+    }, {
+        image: 'assets/img/3.jpg'
+    });
+});
 
-        return{
-            firstName: firstName,
-            lastName: lastName,
-            age: age,
-            email: email,
-            balance: balance
-        };
+myApp.controller('basicsCtrl', function($scope, $http) {
+    var url = "assets/data/vm.json";
+    //$scope.rowCollection = []
+    // Remove Row
+    $scope.removeRow = function removeRow(row) {
+        var index = $scope.rowCollection.indexOf(row);
+        if (index !== -1) {
+            $scope.rowCollection.splice(index, 1);
+        }
     }
+    ;
 
-    scope.itemsByPage = 15;
+    $scope.itemsByPage = 5;
 
-    scope.rowCollection = [];
-    for (var j = 0; j < 200; j++) {
-        scope.rowCollection.push(createRandomItem());
-    }
-}]);
+    $http.get(url).success(function(response) {
+        
+        $scope.rowCollection = response;
+        $scope.abc = url;
+        console.log(11, $scope.rowCollection);
+
+        // Add Owner List
+        var owner_list = [];
+        for (var i = 0; i < $scope.rowCollection.length; i++) {
+            owner_list[i] = $scope.rowCollection[i].owner;
+        }
+
+        function unique(arr) {
+            var result = []
+              , hash = {};
+            for (var i = 0, elem; (elem = arr[i]) != null; i++) {
+                if (!hash[elem]) {
+                    result.push(elem);
+                    hash[elem] = true;
+                }
+            }
+            return result;
+        }
+        var arr = unique(owner_list);
+        $scope.activities = arr;
+    });
+
+});
+
+myApp.directive('stRatio', function() {
+    return {
+        link: function(scope, element, attr) {
+            var ratio = +(attr.stRatio);
+
+            element.css('width', ratio + '%');
+
+        }
+    };
+});
