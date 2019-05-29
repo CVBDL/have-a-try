881c614bd3153482948401db83e3329775eb76f5
shinobi01 <chobitsWQ321321321>
2019-05-29 21:04:17 +0800

    Add Initial display

--- Aaron/PyBaseAutoTest/viewpoint/lib/viewpoint_solution.py
+++ Aaron/PyBaseAutoTest/viewpoint/lib/viewpoint_solution.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import csv
-# def _init():
-#     global _global_dict
-#     _global_dict = {}
-# def _init():
-global file_path
-file_path = "C:\\Users\\awang14\\Desktop\\Merlin\\PyBaseAutoTest\\viewpoint\\tests\\Assets\\_Solution_Config\\Solution_Config.csv"
+import os
 
-def get_value_csv(self, case_name):
-    case_name = 'BAT_SE'
-    with open(file_path) as solution_config:
+
+vp_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+solution_path = vp_path + '/config/Solution_Config.csv'
+
+
+def get_value_csv(case_name):
+    # case_name = 'BAT_SE'
+    with open(solution_path) as solution_config:
         reader = csv.DictReader(solution_config)
         items = list(reader)
         for row in items:
             if row["%CaseName"] == case_name:
-                print(row['%initialDisplayCsv'])
-                print(row['%appTypeCsv'])
-                print("test")
-                return row
+                return row['%initialDisplayCsv']
--- Aaron/PyBaseAutoTest/viewpoint/tests/Python Test/test_01_homepage.py
+++ Aaron/PyBaseAutoTest/viewpoint/tests/Python Test/test_01_homepage.py
@@ -3,11 +3,13 @@ import sys
 sys.path.append('.')
 from viewpoint.tests.base_test import BaseTestCase
 from viewpoint.lib.viewpoint_global import Global
+from viewpoint.lib.viewpoint_solution import get_value_csv
 
 
 @pytest.fixture(scope='module')
 def init_global():
-    Global().caseName = "MerlinTest"
+    Global().caseName = "BAT_SE"
+    Global().initDisplay = get_value_csv(Global().caseName)
 
 
 @pytest.mark.usefixtures("init_global", "publish_application", "util")
